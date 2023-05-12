72dd1bfd4e523e18f32abbb9cdd9d1e35f8fb982
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-12 11:48:21 +0200

    completed

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,7 +1,35 @@
 package org.java;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		
+		ArrayList<String> wishList = new ArrayList<>();
+		
+		while(true) {
+			System.out.println("Cosa desideri");
+			String wish = in.nextLine();
+			wishList.add(wish);
+			
+			System.out.println("Ecco la tua lista:");
+			for (String val : wishList) {
+				System.out.println(val);
+			}
+			
+			System.out.println("1 - vuoi continuare");
+			System.out.println("2 - vuoi uscire");
+			
+			int choise = in.nextInt();
+			if(choise < 1 && choise > 2) {
+				continue;
+			}
+			if(choise == 2) break;
+			in.nextLine();
+		}
+		System.out.println(wishList);
 	}
 }
